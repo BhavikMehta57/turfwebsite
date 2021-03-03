730e3af51237166ef11a8dcd35c5a2d625970729
BhavikMehta57 <shadow7bm@gmail.com>
2021-03-03 16:33:28 +0530

    og

--- turfwebsite/turfwebsite/settings.py
+++ turfwebsite/turfwebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '+mmhq%4=3!^kn+w!5(_g$grn)f2rg6&dku!uuqtw2be+=*b^4x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mygrounds.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
